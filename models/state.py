5889eec62f355922ba03a1b89491c91002a6b81a
juanivalle <juanignaciovalle18@gmail.com>
2023-03-21 14:23:20 -0300

    task 6

--- models/state.py
+++ models/state.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -10,3 +11,4 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete-orphan")
