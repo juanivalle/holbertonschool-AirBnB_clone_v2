3d5484f4b5588947e7ae4d517c53171ee89ccdb9
juanivalle <juanignaciovalle18@gmail.com>
2023-03-23 12:15:07 -0300

    db_storage

---
+++ models/engine/db_storage.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""comments"""
+
+from models.user import User
+from models.base_model import BaseModel, Base
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import create_engine
+from os import getenv
+
+class DBStorage:
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        self.__engine = create_engine(('mysql+mysqldb://{}:{}@{}/{}')
+                                      .format(getenv('HBNB_MYSQL_USER'),
+                                              getenv('HBNB_MYSQL_PWD'),
+                                              getenv('HBNB_MYSQL_HOST'),
+                                              getenv('HBNB_MYSQL_DB')), 
+                                              pool_pre_ping=True)
+        if getenv('HBNB_ENV') == "test":
+            Base.metadata.drop_all(bind=self.__engine)
+
+    def all(self, cls=None):
+        
+
+    def new(self, obj):
+        """comments"""
+        self.__session.add(obj)
+    
+    def save(self):
+        """comments"""
+        self.__session.commit()
+    
+    def delete(self, obj=None):
+        """comments"""
+        self.__session.delete(obj)
+    
+    def reload(self):
+        """comments"""
+        Base.metadata.create_all(self.__engine)
+        ssession = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        ssession = scoped_session(ssession)
+        self.__session = ssession
+
+
+
+                                              
