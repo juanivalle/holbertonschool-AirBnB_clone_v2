d0406c17d6902526c2a986c76fdc2493004c41e3
juanivalle <juanignaciovalle18@gmail.com>
2023-03-27 12:25:59 -0300

    task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -29,8 +29,8 @@ def python(text):
 
 @app.route('/number/<n>', strict_slashes=False)
 def number(n):
-    if n == type(int):
-        return n + " " + "is a number"
+  if type(n) is int:
+    return "n is a number"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
