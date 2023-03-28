2f7f17f0d8a5e0f5b88b58b787646c1b317ccc32
juanivalle <juanignaciovalle18@gmail.com>
2023-03-28 11:41:12 -0300

    task 7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -24,7 +24,7 @@ class DBStorage:
     def __init__(self):
         self.__engine = create_engine(('mysql+mysqldb://{}:{}@{}/{}').format(
             getenv('HBNB_MYSQL_USER'),
-            getenv('HBNB_MYSQL_PWD'), 
+            getenv('HBNB_MYSQL_PWD'),
             getenv('HBNB_MYSQL_HOST'),
             getenv('HBNB_MYSQL_DB')
         ), pool_pre_ping=True)
